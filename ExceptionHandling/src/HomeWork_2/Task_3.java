3377169c5a1f520800f3f969953c0ed68f0a4edb
Елизавета Кузьмина <ekuzmina@TNEWS.local>
2023-06-07 12:23:20 +0300

    Выполнено задание 3 в пакете HomeWork_2

--- ExceptionHandling/src/HomeWork_2/Task_3.java
+++ ExceptionHandling/src/HomeWork_2/Task_3.java
@@ -1,5 +1,4 @@
 package HomeWork_2;
-
 /**
  * Задание 3. Дан следующий код, исправьте его там, где требуется:
  *         public static void main(String[] args) throws Exception {
@@ -23,4 +22,25 @@ package HomeWork_2;
  *         }
  */
 public class Task_3 {
+    public static void main(String[] args) { //Убрать объявление "throws Exception" , т.к. в нём уже есть обработчик исключений.
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Exception ex) { //Заменить Throwable на Exception в блоке catch и переместить в самый конец, т.к.
+            System.out.println("Что-то пошло не так... " + ex); // Throwable может перехватывать исключения, которые не
+        }                                                       // являются наследниками класса Exception.
+    }
+    //В методе printSum убрать объявление throws FileNotFoundException, т.к. при выполнении метода никак не приведет
+    // к FileNotFoundException
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
 }
