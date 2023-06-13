69e0bb0e55c42336e6e68d9aec64e098b10934b9
Елизавета Кузьмина <ekuzmina@TNEWS.local>
2023-06-13 22:45:17 +0300

    редакция кода итогового ДЗ по исключениям

--- ExceptionHandling/src/HomeWork_3/Main.java
+++ ExceptionHandling/src/HomeWork_3/Main.java
@@ -9,39 +9,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Введите данные в 1 строку, согласно шаблону:\nФамилия Имя Отчество датарождения(dd.mm.yyyy) номертелефона пол");
+            System.out.println("Введите данные в 1 строку, согласно шаблону:\nФамилия Имя Отчество дата_рождения(dd.mm.yyyy) номер_телефона пол");
             String input = scanner.nextLine();
-            String[] data = input.split(" ");
+            String[] dataArray = input.split(" ");
 
-            if (data.length != 6) {
-                throw new IllegalArgumentException("Неверное количество данных");
+            if (dataArray.length != 6) {
+                throw new IllegalArgumentException("Количество данных должно состоять из 6 строк!");
             }
 
-            String surname = checkValidName(data[0]);
-            String name = checkValidName(data[1]);
-            String patronymic = checkValidName(data[2]);
-            String birthDate = data[3];
-            String phoneNumber = data[4];
-            String gender = data[5];
+            String surname = checkValidName(dataArray[0]);
+            String name = checkValidName(dataArray[1]);
+            String patronymic = checkValidName(dataArray[2]);
+            String dateBirth = dataArray[3];
+            String phoneNumber = dataArray[4];
+            String gender = dataArray[5];
 
-
-            if (!isValidDate(birthDate)) {
-                throw new IllegalArgumentException("Дата рождения введена неверно");
+            if (!isValidDate(dateBirth)) {
+                throw new IllegalArgumentException("Дата рождения введена некорректно!");
             }
 
             long phone = parsePhoneNumber(phoneNumber);
 
             if (!isValidGender(gender)) {
-                throw new IllegalArgumentException("Неверный пол");
+                throw new IllegalArgumentException("Некорректный пол");
             }
 
             String fileName = surname + ".txt";
             BufferedWriter writer = new BufferedWriter(new FileWriter(
                     "C:\\Users\\ekuzmina\\IdeaProjects\\algorithms\\ExceptionHandling\\src\\HomeWork_3\\" + fileName, true));
             writer.append("<").append(surname).append(">").append("<").append(name).append(">").append("<").append(patronymic)
-                    .append(">").append("<").append(birthDate).append("> ").append("<").append(String.valueOf(phone))
+                    .append(">").append("<").append(dateBirth).append("> ").append("<").append(String.valueOf(phone))
                     .append(">").append("<").append(gender).append(">").append(System.lineSeparator());
             writer.close();
             System.out.println("Данные успешно записаны в файл " + fileName);
@@ -52,40 +50,31 @@ public class Main {
         }
     }
 
-
     private static String checkValidName(String name){
         if (name == null || !name.matches("[А-Яа-яЁёA-Za-z]+")) {
-            throw new IllegalArgumentException("Недопустимые символы в ФИО: " + name);
+            throw new IllegalArgumentException("Некорректные символы в ФИО: " + name);
         }
-
         return name;
     }
 
-
-    private static boolean isValidDate(String date) {
-        // Проверка формата даты
+    private static boolean isValidDate(String date) { // Проверка формата даты
         if (date == null || !date.matches("\\d{2}\\.\\d{2}\\.\\d{4}")) {
             return false;
         }
-
         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
         sdf.setLenient(false);
-
         try {
             sdf.parse(date);
         } catch (ParseException e) {
             return false;
         }
-
         return true;
     }
 
-    private static long parsePhoneNumber(String phoneNumber) {
-        // Парсинг номера телефона
+    private static long parsePhoneNumber(String phoneNumber) { // Парсинг номера телефона
         if (phoneNumber == null || !phoneNumber.matches("\\d+")) {
             throw new IllegalArgumentException("Неверный формат номера телефона");
         }
-
         try {
             return Long.parseLong(phoneNumber);
         } catch (NumberFormatException e) {
@@ -93,13 +82,11 @@ public class Main {
         }
     }
 
-    private static boolean isValidGender(String gender) {
-        // Проверка корректности значения пола
+    private static boolean isValidGender(String gender) { // Проверка корректности значения пола
         if (gender != null && gender.length() == 1 && (gender.equalsIgnoreCase("m") ||
                 gender.equalsIgnoreCase("f"))) return true;
         assert gender != null;
         return gender.equalsIgnoreCase("м") ||
         gender.equalsIgnoreCase("ж");
     }
-
 }
